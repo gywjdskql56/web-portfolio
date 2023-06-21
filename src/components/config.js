5d1474daafca83d834924501233707e55ac2b9ba
hyojeong_kim <gywjdskql5915@gmail.com>
2023-06-21 15:13:43 +0900

    change config

--- src/components/config.js
+++ src/components/config.js
@@ -2,7 +2,7 @@ global.XMLHttpRequest = require("xhr2");
 
 //export const url = "http://172.16.120.19:5001";
 //export const url = "http://172.16.130.244:5001";
-export const url = "http://172.29.46.39:5000";
+export const url = "http://52.79.241.243:5000";
 //export const url2 = "http://192.168.0.9:5001";
 
 
