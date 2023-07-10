f49c87f68fd924ab0ac3cdc1083186e7b6aa969d
hyojeong_kim <gywjdskql5915@gmail.com>
2023-07-10 17:11:45 +0900

    change url

--- src/components/config.js
+++ src/components/config.js
@@ -1,7 +1,7 @@
 global.XMLHttpRequest = require("xhr2");
 
-export const url = "http://172.16.120.19:5000";
-//export const url = "http://52.79.241.243:5000";
+//export const url = "http://172.16.120.19:5000";
+export const url = "http://52.79.241.243:5000";
 
 
 export function httpGet(theURL) {
